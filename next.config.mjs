372cee8c557ae3acb4196ad794a312308a08443b
Dpancha6 <dpancha6@gmail.com>
2024-06-28 17:20:47 -0700

    Configure for GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isProd = process.env.NODE_ENV === 'production';
+const repoName = 'next-tailwind-draggable-list';
+
+const nextConfig = {
+  basePath: isProd ? `/${repoName}` : '',
+  assetPrefix: isProd ? `/${repoName}/` : '',
+  images: {
+    loader: 'default',
+    path: isProd ? `/${repoName}/` : '/',
+  },
+};
 
 export default nextConfig;
